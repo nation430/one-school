c59b4e99cb3c9dd8829f795fa206ef80862b316d
Uzor Mercy <mercy4hope@gmail.com>
2020-08-16 20:19:54 +0100

    Added a new cbt with Api

---
+++ cbt/js/questions.js
@@ -0,0 +1,20 @@
+const results = [
+    {
+        "id":1,
+        "question": "Is Javascript a programming language",
+        "options": ["Maybe", "Absolutly", "Not Sure"],
+        "correct": 1
+    },
+    {
+        "id":2,
+        "question": "Which is a Frontend programming language?",
+        "options": ["JAVASCRIPT", "PHP", "PYTHON"],
+        "correct": 0
+    },
+    {
+        "id":3,
+        "question": "Is Html a programming language",
+        "options": ["True", "False", "Not Sure"],
+        "correct": 0
+    }
+]
